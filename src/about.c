c8e2aa7a06b98b900662855b5de8c9759044ad47
Andreas Rönnquist <gusnan@users.sourceforge.net>
2011-04-17 19:35:01 +0200

    Updated the version number

--- src/about.c
+++ src/about.c
@@ -44,7 +44,7 @@ static gchar *sLicense =	(gchar*)"SciteProj is free software: you can redistribu
 
 gchar *homepage_string=(gchar*)"http://www.gusnan.se/sciteproj";
 
-gchar *sVersion = (gchar*)"0.4.10";
+gchar *sVersion = (gchar*)"0.5.00";
 
 static GtkWidget *window;
 
