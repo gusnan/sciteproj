c4612e3dd935659230ca6c4c91e012e9a503205e
Andreas Rönnquist <gusnan@users.sourceforge.net>
2011-10-17 03:47:02 +0200

    Update with gtk3 compatibility definitions

--- src/gtk3_compat.h
+++ src/gtk3_compat.h
@@ -5,7 +5,14 @@
 #define __HEADER_GTK3_COMPAT_
 
 #ifndef GDK_KEY_Return
-	#define GDK_KEY_Backspace GDK_BackSpace
+	#define GDK_KEY_BackSpace GDK_BackSpace
+	#define GDK_KEY_Delete GDK_Delete
+	#define GDK_KEY_Insert GDK_Insert
+	#define GDK_KEY_Return GDK_Return
+	#define GDK_KEY_KP_Enter GDK_KP_Enter
+	#define GDK_KEY_Escape GDK_Escape
+	#define GDK_KEY_F2 GDK_F2
+	#define GDK_KEY_F5 GDK_F5
 #endif
 
 #endif /*__HEADER_GTK3_COMPAT_*/
