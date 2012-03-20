c01873c79282609d2871727ce612aa536aa5ecb1
Andreas Rönnquist <gusnan@users.sourceforge.net>
2012-03-20 13:17:24 +0100

    Add GPL header to GPL compatibility header

--- src/gtk3_compat.h
+++ src/gtk3_compat.h
@@ -1,8 +1,29 @@
-#include <gtk/gtk.h>
-#include <gdk/gdkkeysyms.h>
-
+/**
+ * gtk3_compat.h - GTK3 compatibility definitions
+ *
+ *  Copyright 2011-2012 Andreas Rönnquist
+ *
+ * This file is part of SciteProj.
+ * 
+ * SciteProj is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SciteProj is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SciteProj.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #ifndef __HEADER_GTK3_COMPAT_
 #define __HEADER_GTK3_COMPAT_
+
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
 #ifndef GDK_KEY_Return
 	#define GDK_KEY_BackSpace GDK_BackSpace
