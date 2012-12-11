1d19669690f4effde17d3d1817766efbac1e16b7
Andreas Rönnquist <gusnan@users.sourceforge.net>
2012-12-11 01:02:17 +0100

    Sort folders and files by themselves

--- src/load_folder.c
+++ src/load_folder.c
@@ -45,6 +45,15 @@ int currentFilePrevFileIter=0;
 gboolean prevFileIterValid[100];
 
 /**
+ *
+ */
+gint file_sort_func(gconstpointer a, gconstpointer b)
+{
+	return g_ascii_strcasecmp(a,b);
+}
+
+
+/**
  * @param store - the treestore where to store the data
  * @param folder_path - the folder to read the contents from
  *
@@ -57,76 +66,119 @@ void read_folder(GtkTreeStore *store, gchar *folder_path,ParseFileStruct *parse_
 	const gchar *short_filename;
 
 	int last_type=0;
-
+	
+	GSList *file_list=NULL;
+	GSList *folder_list=NULL;
+	
 	while ((short_filename = g_dir_read_name(dir))) {
+		
+		gchar *temp_file=g_build_filename(folder_path, short_filename, NULL);
+		
+		if (g_file_test(temp_file, G_FILE_TEST_IS_DIR)) {
+			folder_list=g_slist_prepend(folder_list,(gpointer)short_filename);
+		} else {
+			file_list=g_slist_prepend(file_list,(gpointer)short_filename);
+		}
+	};
+	
+	file_list=g_slist_sort(file_list,file_sort_func);
+	folder_list=g_slist_sort(folder_list,file_sort_func);
+	
+	// Treat folders and files by themselves
+	if (folder_list!=NULL) {
+		while (folder_list!=NULL) {
+			
+			short_filename=(gchar*)(folder_list->data);
+			
+			current_file = g_build_filename(folder_path, short_filename,NULL);
 
-		current_file = g_build_filename(folder_path, short_filename,NULL);
+			if (short_filename[0]!='.') {
+				if (g_file_test(current_file, G_FILE_TEST_IS_DIR)) {
 
-		if (short_filename[0]!='.') {
-			if (g_file_test(current_file, G_FILE_TEST_IS_DIR)) {
+					folder_number++;
 
-				folder_number++;
+					if (parse_file->depth<=0) {
 
-				if (parse_file->depth<=0) {
+						add_tree_group(NULL, ADD_CHILD, short_filename, TRUE, &(parse_file->current_iter), NULL);
 
-					add_tree_group(NULL, ADD_CHILD, short_filename, TRUE, &(parse_file->current_iter), NULL);
+						prevFileIterValid[currentFilePrevFileIter]=FALSE;
+						prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
 
-					prevFileIterValid[currentFilePrevFileIter]=FALSE;
-					prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
+						currentFilePrevFileIter++;
+						prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
 
-					currentFilePrevFileIter++;
-					prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
-
-				} else {
-
-					add_tree_group(&(parse_file->current_iter), ADD_CHILD, short_filename, TRUE, &(parse_file->current_iter), NULL);
-
-					prevFileIterValid[currentFilePrevFileIter]=FALSE;
-					prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
-
-					currentFilePrevFileIter++;
-					prevFileIterValid[currentFilePrevFileIter]=prevFileIterValid[currentFilePrevFileIter];
-				}
-
-				parse_file->depth++;
-
-				prevFileIterValid[currentFilePrevFileIter]=FALSE;
-
-				// We ignore hidden folders
-				if (short_filename[0]!='.') {
-					read_folder(store, current_file, parse_file, error);
-
-				}
-
-				last_type=1;
-
-			} else {
-				// add as a file
-
-				if (parse_file->depth<=0) {
-
-					if (prevFileIterValid[currentFilePrevFileIter]) {
-						add_tree_file(&(prevFileIterArray[currentFilePrevFileIter]), ADD_AFTER, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
 					} else {
-						add_tree_file(NULL, ADD_CHILD, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+
+						add_tree_group(&(parse_file->current_iter), ADD_CHILD, short_filename, TRUE, &(parse_file->current_iter), NULL);
+
+						prevFileIterValid[currentFilePrevFileIter]=FALSE;
+						prevFileIterArray[currentFilePrevFileIter]=parse_file->current_iter;
+
+						currentFilePrevFileIter++;
+						prevFileIterValid[currentFilePrevFileIter]=prevFileIterValid[currentFilePrevFileIter];
 					}
 
-				} else {
+					parse_file->depth++;
 
-					if (prevFileIterValid[currentFilePrevFileIter]) {
-						add_tree_file(&(prevFileIterArray[currentFilePrevFileIter]), ADD_AFTER, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
-					} else {
-						add_tree_file(&(parse_file->current_iter), ADD_CHILD, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+					prevFileIterValid[currentFilePrevFileIter]=FALSE;
+
+					// We ignore hidden folders
+					if (short_filename[0]!='.') {
+						read_folder(store, current_file, parse_file, error);
+
 					}
+
+					last_type=1;
+
 				}
-				
-				prevFileIterValid[currentFilePrevFileIter]=TRUE;
-			}
-		}	
+			}	
 
-		if (folder_number>0) folder_number--;
+			if (folder_number>0) folder_number--;
 
-		g_free(current_file);
+			g_free(current_file);
+			
+			folder_list=folder_list->next;
+		}
+	}
+	
+	if (file_list!=NULL) {
+		while (file_list!=NULL) {
+			
+			short_filename=(gchar*)(file_list->data);
+			
+			current_file = g_build_filename(folder_path, short_filename,NULL);
+
+			if (short_filename[0]!='.') {
+				if (!g_file_test(current_file, G_FILE_TEST_IS_DIR)) {
+					// add as a file
+
+					if (parse_file->depth<=0) {
+
+						if (prevFileIterValid[currentFilePrevFileIter]) {
+							add_tree_file(&(prevFileIterArray[currentFilePrevFileIter]), ADD_AFTER, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+						} else {
+							add_tree_file(NULL, ADD_CHILD, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+						}
+
+					} else {
+
+						if (prevFileIterValid[currentFilePrevFileIter]) {
+							add_tree_file(&(prevFileIterArray[currentFilePrevFileIter]), ADD_AFTER, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+						} else {
+							add_tree_file(&(parse_file->current_iter), ADD_CHILD, current_file, &(prevFileIterArray[currentFilePrevFileIter]), TRUE, error);
+						}
+					}
+					
+					prevFileIterValid[currentFilePrevFileIter]=TRUE;
+				}
+			}	
+
+			if (folder_number>0) folder_number--;
+
+			g_free(current_file);
+			
+			file_list=file_list->next;
+		}
 	}
 	
 	if (currentFilePrevFileIter>0) currentFilePrevFileIter--;
