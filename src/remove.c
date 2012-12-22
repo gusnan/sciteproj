c775778f856b00c502a99f5415d69bfb89a484f3
Andreas Rönnquist <gusnan@users.sourceforge.net>
2012-12-22 14:41:41 +0100

    Move check for empty folder

--- src/remove.c
+++ src/remove.c
@@ -44,34 +44,13 @@ gboolean delete_file(gchar *filename,GError **error)
 {
 	gboolean result=FALSE;
 	
-	printf("delete_file\n");
-	
-	printf("filename: %s\n", filename);
-	
 	gchar *file_to_delete;
 	
 	if (g_file_test(filename, G_FILE_TEST_IS_DIR)) {
-		printf("Foldername: ");
 		file_to_delete=filename;
 			
-		if (get_number_of_files_in_folder(file_to_delete)>0) {
-			
-			GtkWidget *dialog = gtk_message_dialog_new(NULL,
-															GTK_DIALOG_MODAL, 
-															GTK_MESSAGE_QUESTION,
-															GTK_BUTTONS_OK,
-															"Folder need to be empty to be able to delete it!");
-			gtk_dialog_run(GTK_DIALOG(dialog));
-			
-			gtk_widget_destroy(dialog);
-
-			result=FALSE;
-			goto EXITPOINT;
-		}
-		
+		// We have already checked if the folder is empty
 	} else {
-
-		printf("Filename: ");
 		
 		if (!relative_path_to_abs_path(filename, &file_to_delete, get_project_directory(), error)) {
 			result=FALSE;
@@ -408,6 +387,27 @@ void do_remove_node(gboolean ignore_clicked_node)
 			
 			// TODO: check if the folder is empty already here before "confirmation" questions
 			
+			gchar *filepath;
+					
+			GtkTreeModel *tree_model = gtk_tree_view_get_model(GTK_TREE_VIEW(projectTreeView));
+		
+			gtk_tree_model_get(tree_model, &clicked_node.iter, 
+										COLUMN_FILEPATH, &filepath, -1);
+				
+			if (get_number_of_files_in_folder(filepath)>0) {
+				
+				GtkWidget *dialog = gtk_message_dialog_new(NULL,
+																GTK_DIALOG_MODAL, 
+																GTK_MESSAGE_QUESTION,
+																GTK_BUTTONS_OK,
+																"Folder need to be empty to be able to delete it!");
+				gtk_dialog_run(GTK_DIALOG(dialog));
+				
+				gtk_widget_destroy(dialog);
+
+				goto EXITPOINT;
+			}
+
 			
 			if (nodename) {
 				really_do_delete = really_do_delete_question(_("Delete folder '%s' and any contained files?"),nodename);
