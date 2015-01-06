99666661b851063ae3d2940bae0b1ac0acba6b40
Andreas Rönnquist <gusnan@gusnan.se>
2015-01-06 20:27:03 +0100

    Update copyright

--- src/clipboard.c
+++ src/clipboard.c
@@ -1,7 +1,7 @@
 /**
  * clipboard.c - clipboard code for SciteProj
  *
- *  Copyright 2010-2012 Andreas Rönnquist
+ *  Copyright 2010-2015 Andreas Rönnquist
  *
  * This file is part of SciteProj.
  *
