470ffd36605eba9585d477f037fe3bd9b4eb93b6
Andreas Rönnquist <gusnan@librem.one>
2023-03-07 15:11:59 +0100

    Remove newline in GPL header

--- src/tree_manipulation.h
+++ src/tree_manipulation.h
@@ -16,8 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SciteProj.
- * If not, see <http://www.gnu.org/licenses/>.
+ * along with SciteProj.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #ifndef __HEADER_TREE_MANIPULATION_
